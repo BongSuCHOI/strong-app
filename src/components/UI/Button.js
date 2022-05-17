acfc4451f1844c2455dc35261220652c9e446c81
BongSuCHOI <76158466+BongSuCHOI@users.noreply.github.com>
2022-05-17 18:44:48 +0900

    update Button Component props

--- src/components/UI/Button.js
+++ src/components/UI/Button.js
@@ -2,16 +2,22 @@ import styled from "styled-components";
 
 const Btn = styled.button`
 	width: 100%;
-	height: ${(props) => (props.small ? "25px" : "40px")};
+	height: ${(props) => (props.small ? "30px" : "40px")};
+	margin: ${(props) => (props.margin ? props.margin : "0")};
 	text-align: center;
+	font-weight: 500;
+	font-size: ${(props) => (props.small ? "14px" : "16px")};
 	color: ${(props) => (props.theme === "gray" ? "#000" : "#fff")};
 	background: ${(props) => (props.theme === "gray" ? "#eaeaea" : "#35a7ff")};
 	border-radius: 6px;
-	font-size: 16px;
 `;
 
 function Button(props) {
-	return <Btn>{props.children}</Btn>;
+	return (
+		<Btn small={props.small} theme={props.theme} margin={props.margin}>
+			{props.children}
+		</Btn>
+	);
 }
 
 export default Button;
