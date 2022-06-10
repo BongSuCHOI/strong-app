41acafe5e6f391f1f6d076d127b31c7ab3d9fb5c
BongSuCHOI <76158466+BongSuCHOI@users.noreply.github.com>
2022-06-10 14:25:37 +0900

    fix: console font관련 경고 및 font file 임포트 방식 변경

--- src/styles/global.js
+++ src/styles/global.js
@@ -1,71 +1,83 @@
 import { createGlobalStyle } from "styled-components";
 
+import NotoSansKr700Woff2 from "../assets/font/noto-sans-kr-700.woff2";
+import NotoSansKr700Woff from "../assets/font/noto-sans-kr-700.woff";
+import NotoSansKr700Eot from "../assets/font/noto-sans-kr-700.eot";
+import NotoSansKr500Woff2 from "../assets/font/noto-sans-kr-500.woff2";
+import NotoSansKr500Woff from "../assets/font/noto-sans-kr-500.woff";
+import NotoSansKr500Eot from "../assets/font/noto-sans-kr-500.eot";
+import NotoSansKr400Woff2 from "../assets/font/noto-sans-kr-400.woff2";
+import NotoSansKr400Woff from "../assets/font/noto-sans-kr-400.woff";
+import NotoSansKr400Eot from "../assets/font/noto-sans-kr-400.eot";
+import Roboto700Woff2 from "../assets/font/roboto-700.woff2";
+import Roboto700Woff from "../assets/font/roboto-700.woff";
+import Roboto700Eot from "../assets/font/roboto-700.eot";
+import Roboto700Ttf from "../assets/font/roboto-700.ttf";
+import Roboto400Woff2 from "../assets/font/roboto-400.woff2";
+import Roboto400Woff from "../assets/font/roboto-400.woff";
+import Roboto400Eot from "../assets/font/roboto-400.eot";
+import Roboto400Ttf from "../assets/font/roboto-400.ttf";
+
 const GlobalStyle = createGlobalStyle`
-    /* roboto-regular - latin */
     @font-face {
         font-family: "Roboto";
         font-style: normal;
         font-weight: 400;
-        src: url("../assets/font/roboto-v30-latin-regular.eot");
+        src: url(${Roboto400Eot});
         src: local(""),
-            url("../assets/font/roboto-v30-latin-regular.eot?#iefix") format("embedded-opentype"),
-            url("../assets/font/roboto-v30-latin-regular.woff2") format("woff2"),
-            url("../assets/font/roboto-v30-latin-regular.woff") format("woff"),
-            url("../assets/font/roboto-v30-latin-regular.svg#Roboto") format("svg");
+            url(${Roboto400Eot}?#iefix) format("embedded-opentype"),
+            url(${Roboto400Woff2}) format("woff2"),
+            url(${Roboto400Woff}) format("woff"),
+            url(${Roboto400Ttf}) format("truetype"),
         unicode-range: U+0041-005A, U+0061-007A;
     }
-    /* roboto-700 - latin */
+
     @font-face {
         font-family: "Roboto";
         font-style: normal;
         font-weight: 700;
-        src: url("../assets/font/roboto-v30-latin-700.eot");
-        src: local(""), url("../assets/font/roboto-v30-latin-700.eot?#iefix") format("embedded-opentype"),
-            url("../assets/font/roboto-v30-latin-700.woff2") format("woff2"),
-            url("../assets/font/roboto-v30-latin-700.woff") format("woff"),
-            url("../assets/font/roboto-v30-latin-700.svg#Roboto") format("svg");
+        src: url(${Roboto700Eot});
+        src: local(""),
+            url(${Roboto700Eot}?#iefix) format("embedded-opentype"),
+            url(${Roboto700Woff2}) format("woff2"),
+            url(${Roboto700Woff}) format("woff"),
+            url(${Roboto700Ttf}) format("truetype"),
         unicode-range: U+0041-005A, U+0061-007A;
     }
 
-    /* noto-sans-kr-regular */
     @font-face {
-        font-display: swap;
         font-family: "Noto Sans KR";
         font-style: normal;
         font-weight: 400;
-        src: url("../assets/font/noto-sans-kr-400.eot");
-        src: local(""), url("../assets/font/noto-sans-kr-400.eot?#iefix") format("embedded-opentype"),
-            url("../assets/font/noto-sans-kr-400.woff2") format("woff2"),
-            url("../assets/font/noto-sans-kr-400.woff") format("woff"),
-            url("../assets/font/noto-sans-kr-400.svg#NotoSansKR") format("svg");
+        src: url(${NotoSansKr400Eot});
+        src: local(""),
+            url(${NotoSansKr400Eot}?#iefix) format("embedded-opentype"),
+            url(${NotoSansKr400Woff2}) format("woff2"),
+            url(${NotoSansKr400Woff}) format("woff"),
         unicode-range: U+AC00-D7A3, U+0030-0039;
     }
 
-    /* noto-sans-kr-medium */
     @font-face {
-        font-display: swap;
         font-family: "Noto Sans KR";
         font-style: normal;
         font-weight: 500;
-        src: url("../assets/font/noto-sans-kr-500.eot");
-        src: local(""), url("../assets/font/noto-sans-kr-500.eot?#iefix") format("embedded-opentype"),
-            url("../assets/font/noto-sans-kr-500.woff2") format("woff2"),
-            url("../assets/font/noto-sans-kr-500.woff") format("woff"),
-            url("../assets/font/noto-sans-kr-500.svg#NotoSansKR") format("svg");
+        src: url(${NotoSansKr500Eot});
+        src: local(""),
+            url(${NotoSansKr500Eot}?#iefix) format("embedded-opentype"),
+            url(${NotoSansKr500Woff2}) format("woff2"),
+            url(${NotoSansKr500Woff}) format("woff"),
         unicode-range: U+AC00-D7A3, U+0030-0039;
     }
 
-    /* noto-sans-kr-bold */
     @font-face {
-        font-display: swap;
         font-family: "Noto Sans KR";
         font-style: normal;
         font-weight: 700;
-        src: url("../assets/font/noto-sans-kr-700.eot");
-        src: local(""), url("../assets/font/noto-sans-kr-700.eot?#iefix") format("embedded-opentype"),
-            url("../assets/font/noto-sans-kr-700.woff2") format("woff2"),
-            url("../assets/font/noto-sans-kr-700.woff") format("woff"),
-            url("../assets/font/noto-sans-kr-700.svg#NotoSansKR") format("svg");
+        src: url(${NotoSansKr700Eot});
+        src: local(""),
+            url(${NotoSansKr700Eot}?#iefix) format("embedded-opentype"),
+            url(${NotoSansKr700Woff2}) format("woff2"),
+            url(${NotoSansKr700Woff}) format("woff"),
         unicode-range: U+AC00-D7A3, U+0030-0039;
     }
 
